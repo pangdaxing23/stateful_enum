d55bafa0a8af8dca3e29e535863cec267d8384f9
Akira Matsuda <ronnie@dio.jp>
2025-05-02 05:08:36 +0900

    [CI] User is also directly invoked via graph_test.rb
    so this model has to cover both old and new enum syntax

--- test/dummy/app/models/user.rb
+++ test/dummy/app/models/user.rb
@@ -1,15 +1,29 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
-  enum :account_status, {active: 0, withdrawn: 1} do
-    event :withdraw do
-      transition :active => :withdrawn
+  if Rails::VERSION::MAJOR >= 7
+    enum :account_status, {active: 0, withdrawn: 1} do
+      event :withdraw do
+        transition :active => :withdrawn
+      end
     end
-  end
 
-  enum :player_status, {normal: 0, poison: 1, dead: 2} do
-    event :die do
-      transition [:normal, :poison] => :dead
+    enum :player_status, {normal: 0, poison: 1, dead: 2} do
+      event :die do
+        transition [:normal, :poison] => :dead
+      end
+    end
+  else
+    enum account_status: {active: 0, withdrawn: 1} do
+      event :withdraw do
+        transition :active => :withdrawn
+      end
+    end
+
+    enum player_status: {normal: 0, poison: 1, dead: 2} do
+      event :die do
+        transition [:normal, :poison] => :dead
+      end
     end
   end
 end
